23b5ee93b01b03f7a34a1073be6b2814c3f9415e
Stefan Frijters <stefan.frijters@gmail.com>
2023-09-23 09:48:54 +0200

    Use remote oh-my-posh config

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-oh-my-posh init pwsh --config '~\ohmyposh.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Heufneutje/WindowsConfigs/main/ohmyposh.omp.json' | Invoke-Expression
 Import-Module posh-git
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
